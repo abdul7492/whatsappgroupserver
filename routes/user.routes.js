420398e7094fc97d084cc7019a8dba0be58024a5
abdul7492 <abdul7492rehman@gmail.com>
2025-05-11 04:32:39 -0700

    Update user.routes.js
    111

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,13 +2,16 @@ import express from 'express';
 import {
     submitEntry,
     getPlaninfo,
-    setwinners,
+    setwinner,
     getwinners,
+    getusers,
     rest,
 } from '../controllers/user.controller.js'; 
 import { upload, handleMulterError } from '../middlewares/multer.middleware.js';
 
 const router = express.Router();
+
+router.get('/getuser', getusers);
 
 router.get('/info/:amount', getPlaninfo);
 
@@ -16,8 +19,8 @@ router.get('/getwinners', getwinners );
 
 router.post('/addentry', upload, handleMulterError, submitEntry);
 
-router.put('/setwinners', setwinners);
+router.put('/setwinner/:id', setwinner);
 
-router.put('/rest', rest);
+router.put('/reset', rest);
 
 export default router;
