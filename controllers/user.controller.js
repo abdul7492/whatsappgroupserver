4e9553dc65e84831d7a728e2c5f63490de2f8dfd
abdul7492 <abdul7492rehman@gmail.com>
2025-05-11 05:55:38 -0700

    Update user.controller.js
    msvdc

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -118,4 +118,21 @@ export const setwinner = async (req, res) => {
       res.status(500).json({ error: 'Server error, could not reset entries.' });
     }
   };
+
+
+ export const deleteUser = async (req, res) => {
+    try {
+      const { id } = req.params;
   
+      const deletedUser = await User.findByIdAndDelete(id);
+  
+      if (!deletedUser) {
+        return res.status(404).json({ error: 'User not found.' });
+      }
+  
+      res.json({ message: 'User deleted successfully.', user: deletedUser });
+    } catch (error) {
+      console.error('Error deleting user:', error);
+      res.status(500).json({ error: 'Server error, could not delete user.' });
+    }
+  };
